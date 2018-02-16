cc06dd9b47c2b955a27d37981ba3b8734d5810ae
Ayisien <alan.dorlean@gmail.com>
2018-02-16 10:40:24 +0100

    Adding files
    1- New index.php
    2- New .travis.yml
    3- Adding status into README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 5AIW2-adorlean-indusDev
+[![Build Status](https://travis-ci.org/AlanDNG/5AIW2-adorlean-indusDev.svg?branch=master)](https://travis-ci.org/AlanDNG/5AIW2-adorlean-indusDev)
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alan Dorlean</title>
+</head>
+<body>
+	<?php 
+		$world = "world";
+	?>
+	<h1>Hello <?php echo $world ?> !</h1>
+</body>
+</html>
